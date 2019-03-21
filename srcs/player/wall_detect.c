480c45c890e8ee0725511ccc349d6665a167061e
jchardin <jerome.chardin@outook.com>
2019-03-21 15:19:58 +0100

    changement dans wall detect

--- srcs/player/wall_detect.c
+++ srcs/player/wall_detect.c
@@ -6,25 +6,25 @@
 /*   By: naali <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/18 16:31:21 by naali             #+#    #+#             */
-/*   Updated: 2019/03/21 14:46:50 by jchardin         ###   ########.fr       */
+/*   Updated: 2019/03/21 15:17:29 by jchardin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wolf3d.h"
-void		static wall_function(t_print *w, double dist, double x);
 
-double		dist_calc(double xa, double ya, double xb, double yb)
+
+void		static ft_draw_wall(t_print *w, double dist, double x)
 {
-	double	dist;
-	double	x;
-	double	y;
+	t_vertex	w_up;
+	t_vertex	w_bot;
+	double		hmp;// hauteur du mur percu par le joueur.
 
-	x = (xb - xa) * (xb - xa);
-	y = (yb - ya) * (yb - ya);
-	dist = sqrt(x + y);
-	return (dist);
+	hmp = (((double)EYE * (double)WALL) / dist) / 2.0;
+	w_up = init_vtex(x, ((double)WINY / 2.0) - hmp, 0);
+	w_bot = init_vtex(x, ((double)WINY / 2.0) + hmp, 0);
+	SDL_SetRenderDrawColor(w->renderer_3d, 127, 0, 127, 75);
+	print_line(w, w_up, w_bot);
 }
-
 
 int		static ft_colision_detection(t_map *m, int tmp_x, int tmp_y)
 {
@@ -37,50 +37,40 @@ int		static ft_colision_detection(t_map *m, int tmp_x, int tmp_y)
 	return (FALSE);
 }
 
-void		wall_detect(t_print *w, t_player *p, t_map *m, double alpha, int window_x)
-{ double		dist; double		distmax;
-	double		x;
-	double		y;
-	int tmp_x;
-	int tmp_y;
-	int colision;
+double		static dist_calc(double xa, double ya, double xb, double yb)
+{
+	double	dist;
+	double	x;
+	double	y;
 
-	dist = 0;
-	colision = FALSE;
-	distmax = 99999; //m->xcase * 2;
-	x = 0;
-	y = 0;
-	while (dist < distmax && colision == FALSE)
-	{
-		x = cos(conv_deg_to_rad(alpha - 90)) * dist;
-		y = sin(conv_deg_to_rad(alpha - 90)) * dist;
-		tmp_x = p->pos.x + x;
-		tmp_y = p->pos.y + y;
-		if ((colision = ft_colision_detection(m, tmp_x, tmp_y)) == FALSE)
-		{
-			SDL_RenderDrawPoint(w->ren, tmp_x, tmp_y);
-			dist++;
-		}
-		else
-		{		
-			dist = dist_calc(p->pos.x, p->pos.y, tmp_x, tmp_x);
-			if (dist > 0)
-				wall_function(w, dist, (double)window_x);
-		}
-	}
+	x = (xb - xa) * (xb - xa);
+	y = (yb - ya) * (yb - ya);
+	dist = sqrt(x + y);
+	return (dist);
 }
 
-void		static wall_function(t_print *w, double dist, double x)
-{
-	t_vertex	w_up;
-	t_vertex	w_bot;
-	double		hmp;// hauteur du mur percu par le joueur.
+static void		 wall_detect(t_print *w, t_player *p, t_map *m, double alpha, int window_x)
+{ 
+	double		ray_distance;
+   	double		ray_distance_max;
+	double		x;
+	double		y;
+	int			colision;
 
-	hmp = (((double)EYE * (double)WALL) / dist) / 2.0;
-	w_up = init_vtex(x, ((double)WINY / 2.0) - hmp, 0);
-	w_bot = init_vtex(x, ((double)WINY / 2.0) + hmp, 0);
-	SDL_SetRenderDrawColor(w->renderer_3d, 127, 0, 127, 75);
-	print_line(w, w_up, w_bot);
+	ray_distance = 0;
+	colision = FALSE;
+	ray_distance_max = 99999;
+	while (ray_distance < ray_distance_max && colision == FALSE)
+	{
+		x = (cos(conv_deg_to_rad(alpha - 90)) * ray_distance) + p->pos.x;
+		y = (sin(conv_deg_to_rad(alpha - 90)) * ray_distance) + p->pos.y;
+		if ((colision = ft_colision_detection(m, x, y)) == FALSE)
+			SDL_RenderDrawPoint(w->ren, x, y);
+		else
+			if ((ray_distance = dist_calc(p->pos.x, p->pos.y, y, y)) > 0)
+				ft_draw_wall(w, ray_distance, (double)window_x);
+		ray_distance++;
+	}
 }
 
 void		ft_raycast(t_print *w, t_player *p, t_map *m, int alpha)
@@ -96,9 +86,8 @@ void		ft_raycast(t_print *w, t_player *p, t_map *m, int alpha)
 	max = angle + 60;
 	SDL_SetRenderDrawColor(w->ren, 0, 255, 0, 50);
 	while (angle < max && window_x < WINX)
-	{
-		wall_detect(w, p, m, angle, window_x);
-		angle += step;
-		window_x++;
+	{ 
+		wall_detect(w, p, m, angle, window_x); 
+		angle += step; window_x++;
 	}
 }
