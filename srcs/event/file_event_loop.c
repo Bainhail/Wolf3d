0b273bce41122c194269b2b085be41f959f939ac
jchardin <jerome.chardin@outook.com>
2019-03-27 12:23:30 +0100

    norme

--- srcs/event/file_event_loop.c
+++ srcs/event/file_event_loop.c
@@ -6,13 +6,13 @@
 /*   By: jchardin <jerome.chardin@outlook.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/16 12:19:33 by jchardin          #+#    #+#             */
-/*   Updated: 2019/03/27 12:18:40 by jchardin         ###   ########.fr       */
+/*   Updated: 2019/03/27 12:23:14 by jchardin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <wolf3d.h>
 
-void			ft_move_player(int move, t_print *w)
+void			ft_move(int move, t_print *w)
 {
 	if (move == UP)
 	{
@@ -35,22 +35,18 @@ void			ft_move_player(int move, t_print *w)
 	refresh_screen(w);
 }
 
-void			ft_update_event_editor(t_my_event *s_event, t_print *w)
+void			ft_update_event_editor(t_my_event *s_event, t_print *w, SDL_Event *event)
 {
-	SDL_Event event;
 
-	while (SDL_PollEvent(&event))
+	if (event->type == SDL_QUIT)
+		s_event->quit = SDL_TRUE;
+	else if (event->type == SDL_KEYDOWN)
 	{
-		if (event.type == SDL_QUIT)
-			s_event->quit = SDL_TRUE;
-		else if (event.type == SDL_KEYDOWN)
-		{
-			event.key.keysym.scancode == SDL_SCANCODE_W ? ft_move_player(UP, w): 0;
-			event.key.keysym.scancode == SDL_SCANCODE_S ? ft_move_player(DOWN, w): 0;
-			event.key.keysym.scancode == SDL_SCANCODE_D ? ft_move_player(TRIGO, w) : 0;
-			event.key.keysym.scancode == SDL_SCANCODE_A ? ft_move_player(ANTI, w) : 0;
-			s_event->key[event.key.keysym.scancode] = SDL_TRUE;
-		}
+		event->key.keysym.scancode == SDL_SCANCODE_W ? ft_move(UP, w): 0;
+		event->key.keysym.scancode == SDL_SCANCODE_S ? ft_move(DOWN, w): 0;
+		event->key.keysym.scancode == SDL_SCANCODE_D ? ft_move(TRIGO, w) : 0;
+		event->key.keysym.scancode == SDL_SCANCODE_A ? ft_move(ANTI, w) : 0;
+		s_event->key[event->key.keysym.scancode] = SDL_TRUE;
 	}
 }
 
@@ -66,12 +62,14 @@ void			ft_init_event_editor(t_my_event *s_event)
 void			ft_event_loop(t_print *w)
 {
 	t_my_event		s_event;
+	SDL_Event 		event;
 
 	ft_init_event_editor(&s_event);
 	s_event.quit = SDL_FALSE;
 	while (!s_event.quit)
 	{
-		ft_update_event_editor(&s_event, w);
+		while (SDL_PollEvent(&event))
+			ft_update_event_editor(&s_event, w, &event);
 		s_event.key[SDL_SCANCODE_ESCAPE] ? s_event.quit = SDL_TRUE : 0;
 		ft_init_event_editor(&s_event);
 		SDL_Delay(2);
