0b55ddfc1fae51ebcf00438bd185992df6fe308f
jchardin <jerome.chardin@outook.com>
2019-03-30 16:56:01 +0100

    affichage a l'endroit des textures

--- srcs/draw/draw_wall.c
+++ srcs/draw/draw_wall.c
@@ -6,7 +6,7 @@
 /*   By: naali <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/29 14:55:35 by naali             #+#    #+#             */
-/*   Updated: 2019/03/29 14:57:17 by naali            ###   ########.fr       */
+/*   Updated: 2019/03/30 16:55:20 by jchardin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,22 @@ void			ft_init_texture_wall_position(t_map *m, t_my_raycast *s_raycast)
 {
 	int le_delta;
 
-	if (s_raycast->orientation == EAST_WALL \
-		|| s_raycast->orientation == WEST_WALL)
-		le_delta = (int)s_raycast->y % (int)m->ycase;
+	if (s_raycast->orientation == EAST_WALL || s_raycast->orientation == WEST_WALL)
+	{
+		if (s_raycast->orientation == EAST_WALL)
+			le_delta = (int)s_raycast->y % (int)m->ycase;
+		else
+			le_delta = (int)m->ycase - ((int)s_raycast->y % (int)m->ycase);
+
+	}
 	else
+	{
+		if (s_raycast->orientation == NORTH_WALL)
 		le_delta = (int)(s_raycast->x + 1.0) % (int)m->xcase;
+		else
+		le_delta =(int)m->xcase - ( (int)(s_raycast->x + 1.0) % (int)m->xcase);
+
+	}
 	s_raycast->srcrect.x = le_delta;
 	s_raycast->srcrect.y = 0;
 	s_raycast->srcrect.w = 1;
@@ -40,7 +51,7 @@ void			ft_draw_wall(t_print *w, t_map *m, t_my_raycast *rc)
 	distance_ray = (rc->dx >= 0 && rc->dx < rc->dy) ? rc->dx : rc->dy;
 	distance_ray = recalc_ray_distance(distance_ray, rc->window_x);
 	rc->orientation = (rc->dx >= 0 && rc->dx < rc->dy) \
-							? X_COLISION : Y_COLISION;
+					  ? X_COLISION : Y_COLISION;
 	rc->orientation = ft_get_wall_orientation(rc->angle, rc->orientation);
 	rc->hmp = (((double)EYE * (double)WALL) / distance_ray) / 2.0;
 	w_up = init_vtex(rc->window_x, ((double)WINY / 2.0) - rc->hmp, 0);
