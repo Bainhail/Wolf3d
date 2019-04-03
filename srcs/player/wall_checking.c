1d3ca86ba7ff5c0ea0a78ac4720ca88e10cb70e8
jchardin <jerome.chardin@outook.com>
2019-04-03 15:37:05 +0200

    nomage

--- srcs/player/wall_checking.c
+++ srcs/player/wall_checking.c
@@ -6,7 +6,7 @@
 /*   By: naali <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/29 14:41:27 by naali             #+#    #+#             */
-/*   Updated: 2019/04/03 15:31:12 by jchardin         ###   ########.fr       */
+/*   Updated: 2019/04/03 15:36:30 by jchardin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,8 +39,6 @@ double			wall_x_detect(t_print *s_win, t_player *player, t_map *map, t_my_raycas
 		//		if (rc->y < player->wl.ymin)  //comprend pas
 		//			player->wl.ymin = rc->y;  //comprend pas
 
-
-
 		rc->colision = ft_colision_detection(&(s_win->m), rc, player->wl.dirx, player->wl.diry);
 		if(rc->colision == TRUE)
 		{
@@ -53,8 +51,6 @@ double			wall_x_detect(t_print *s_win, t_player *player, t_map *map, t_my_raycas
 	}
 	return (rc->dist_col_x);
 }
-
-
 
 static void		init_wall_y(t_player *player, t_map *map, t_my_raycast *rc)
 {
@@ -88,63 +84,3 @@ double			wall_y_detect(t_print *s_win, t_player *player, t_map *map, t_my_raycas
 	}
 	return (rc->dist_col_y);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//static double	check_colision(t_print *s_win, t_player *player, t_my_raycast *rc)
-//{
-//	double		dist;
-//
-//	dist = -1;
-//	if ((rc->colision = ft_colision_detection(&(s_win->m), rc, player->wl.dirx, player->wl.diry)) == FALSE)
-//		SDL_RenderDrawPoint(s_win->renderer[MAP_2D], rc->x, rc->y);
-//	else
-//	{
-//		if ((dist = dist_calc(player->pos.x, player->pos.y, rc->x, rc->y)) > 0)
-//			SDL_RenderDrawPoint(s_win->renderer[MAP_2D], rc->x, rc->y);
-//	}
-//	return (dist);
-//}
