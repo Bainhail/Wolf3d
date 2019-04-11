f723beef1104ed87f0c63b9c4d000491c0b79b7b
jchardin <jerome.chardin@outook.com>
2019-04-11 16:16:59 +0200

    ok

--- srcs/draw/draw_wall.c
+++ srcs/draw/draw_wall.c
@@ -6,7 +6,7 @@
 /*   By: naali <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/29 14:55:35 by naali             #+#    #+#             */
-/*   Updated: 2019/04/11 15:23:16 by jchardin         ###   ########.fr       */
+/*   Updated: 2019/04/11 16:16:41 by jchardin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void			ft_init_texture_wall_position(t_map *m, t_my_raycast *s_raycast)
 			le_delta = (int)s_raycast->y % (int)m->ycase;
 		else
 			le_delta = (int)m->ycase - ((int)s_raycast->y % (int)m->ycase);
-	s_raycast->dstrect.w = m->ycase;//a modifier pour adapter la largeur
+		s_raycast->dstrect.w = m->ycase;//a modifier pour adapter la largeur
 	}
 	else
 	{
@@ -30,7 +30,7 @@ void			ft_init_texture_wall_position(t_map *m, t_my_raycast *s_raycast)
 			le_delta = (int)(s_raycast->x + 1.0) % (int)m->xcase;
 		else
 			le_delta =(int)m->xcase - ((int)(s_raycast->x + 1.0) % (int)m->xcase);
-	s_raycast->dstrect.w = m->xcase;//a modifier pour adapter la largeur
+		s_raycast->dstrect.w = m->xcase;//a modifier pour adapter la largeur
 	}
 	s_raycast->srcrect.x = le_delta / 2;
 	s_raycast->srcrect.w = 1;//Peut etre une modification ici aussi
@@ -40,13 +40,11 @@ void			ft_init_texture_wall_position(t_map *m, t_my_raycast *s_raycast)
 	s_raycast->dstrect.w = 1;//m->xcase;//a modifier pour adapter la largeur
 	s_raycast->dstrect.h = (int)(s_raycast->hmp * 2);
 	s_raycast->dstrect.y = (int)((double)WINY / 2.0) - (s_raycast->hmp/* / 2.0 */);
-
-
-/* // printf("x=%d delta=%d y=%d h=%d orien= %d\n", s_raycast->dstrect.x, */
-/* 									le_delta, */
-/* 									s_raycast->dstrect.y, */
-/* 									s_raycast->dstrect.h, */
-/* 									s_raycast->orientation); */
+	/* // printf("x=%d delta=%d y=%d h=%d orien= %d\n", s_raycast->dstrect.x, */
+	/* 									le_delta, */
+	/* 									s_raycast->dstrect.y, */
+	/* 									s_raycast->dstrect.h, */
+	/* 									s_raycast->orientation); */
 
 }
 
@@ -55,10 +53,10 @@ void			ft_draw_wall(t_print *w, t_map *m, t_my_raycast *rc)
 	t_vertex	w_up;
 	t_vertex	w_bot;
 	double		distance_ray;
+	float precision = 0.1;
 
-	if ((int)(rc->dist_col_x * 10) == (int)(rc->dist_col_y * 10))
-	{
-	}
+	if ((rc->dist_col_x + precision) > rc->dist_col_y && (rc->dist_col_x - precision) < rc->dist_col_y  )
+		;
 	else
 	{
 		rc->orientation = (rc->dist_col_x < rc->dist_col_y)  ? X_COLISION : Y_COLISION;
@@ -66,18 +64,10 @@ void			ft_draw_wall(t_print *w, t_map *m, t_my_raycast *rc)
 	}
 	distance_ray = (rc->dist_col_x < rc->dist_col_y) ? rc->dist_col_x : rc->dist_col_y;
 	distance_ray = recalc_ray_distance(distance_ray, rc->window_x);
-
 	rc->hmp = (((double)EYE * (double)WALL) / distance_ray) / 2.0;
-
-
-
 	w_up = init_vtex(rc->window_x, ((double)WINY / 2.0) - rc->hmp, 0);
 	w_bot = init_vtex(rc->window_x, ((double)WINY / 2.0) + rc->hmp, 0);
-
-
 	SDL_SetRenderDrawColor(w->renderer[MAP_3D], 50, 50, 200, 75);
-
-
 	print_line(w, w->renderer[MAP_3D], init_vtex(rc->window_x, 0, 0), w_up);
 	SDL_SetRenderDrawColor(w->renderer[MAP_3D], 200, 200, 200, 75);
 	print_line(w, w->renderer[MAP_3D], w_bot, init_vtex(rc->window_x, WINY, 0));
